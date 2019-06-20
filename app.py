7454f604e78094cce978d9a935a1ea95a88b0ef3
Felix Backhouse <felix.backhouse@gmail.com>
2019-06-20 14:34:13 +1200

    Initial Commot of Files

---
+++ app.py
@@ -0,0 +1,30 @@
+# https://www.tutorialspoint.com/flask/flask_sqlite.htm
+# http://flask.pocoo.org/docs/0.12/patterns/sqlite3/
+# https://github.com/stevedunford/NZVintageRadios
+
+import sqlite3
+from flask import Flask, Response, render_template, abort
+
+# Creates a Flask object called 'app' that we can use throughout the programme
+app = Flask(__name__)
+
+# This is the function that controls the main page of the web site
+@app.route("/")
+def index():
+  return render_template('home.html',
+                          title="My Application")
+
+# This is the function shows the Athletes page
+@app.route("/plan")
+def plan():
+  return render_template('plan.html', title="Planning")
+
+
+
+# This function deals with any missing pages and shows the Error page
+#@app.errorhandler(404)
+#def page_not_found(e):
+#  return render_template('404.html', title="404"), 404
+#
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,32 @@
+var mymap = L.map('mapid').setView([0, 0], 2);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+	maxZoom: 20, /* changed from original 18*/
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	id: 'mapbox.satellite'
+}).addTo(mymap);
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+mymap.on('click', onMapClick);
+
+
+
+
+$( function() {
+	$( "#slider-vertical" ).slider({
+		orientation: "vertical",
+		range: "min",
+		min: 0,
+		max: 100,
+		value: 60,
+		slide: function( event, ui ) {
+			$( "#amount" ).val( ui.value );
+		}
+    });
+	$( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+} );
