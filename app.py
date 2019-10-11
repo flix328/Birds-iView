782616319745e42c1edd552d435c7a9c4595a4db
Felix Backhouse <felix.backhouse@gmail.com>
2019-10-11 10:41:36 +0000

    Initial attempt to create a database.

--- app.py
+++ app.py
@@ -2,11 +2,21 @@
 # http://flask.pocoo.org/docs/0.12/patterns/sqlite3/
 # https://github.com/stevedunford/NZVintageRadios
 
-import sqlite3
 from flask import Flask, render_template, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 # Creates a Flask object called 'app' that we can use throughout the programme
 app = Flask(__name__)
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="flix328",
+    password="59AV8wYEq@beyN6",
+    hostname="flix328.mysql.pythonanywhere-services.com",
+    databasename="flix328$birds"
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+birds_db = SQLAlchemy(app)
 
 # This is the function that controls the main page of the web site
 @app.route("/")
@@ -40,12 +50,12 @@ from interface import *
 def polygon_add():
 	data = request.args['poly']
 	altitude, heading, overlap, resolution, view_angle, poly = format_input_data(data)
-	
+
 	lat = float(request.args['lat'])
 	lon = float(request.args['lng'])
 	_id = float(request.args['_id'])
 	p = Objects_2D.Point(lat, lon)
-	
+
 	poly.push(_id, p)
 	return jsonify(poly.listify())
 
@@ -55,28 +65,28 @@ MAX_POINTS = 99
 def get_path():
 	data = request.args['data']
 	altitude, heading, overlap, resolution, view_angle, poly = format_input_data(data)
-	
+
 	gs = [Objects_GPS.Point(lat, lng) for _, lat, lng in poly.listify()]
 	# convert GPS points to 2D points
 	p3Ds = [gps_to_xyz(g) for g in gs]
 	p_r = EARTH.project(centre_point(p3Ds))
-	ps = [xyz_to_xy(p_r, p) for p in p3Ds]	
-	
+	ps = [xyz_to_xy(p_r, p) for p in p3Ds]
+
 	camera = {"view angle": view_angle, "resolution": resolution}
-	
+
 	flight_plan = generate_flight_plan(ps, camera, altitude, overlap, heading, max_points=MAX_POINTS)
-	
+
 	poly_2D = [xyz_to_xy(p_r, gps_to_xyz(Objects_GPS.Point(lat, lon))) for _, lat, lon in poly.listify()]
-	
+
 	total = 0
 	for p1_num, p1 in enumerate(poly_2D):
 		p0 = poly_2D[p1_num - 1]
 		total += p0.x * p1.y - p1.x * p0.y
 	area = 0.5 * abs(total)
-	
+
 	dist = flight_path_dist(flight_plan[:MAX_POINTS])
 	num_photos = len(flight_plan)
-	
+
 	result = "{}, {}, {}".format(area, dist, num_photos)
 	for p in flight_plan:
 		g = xy_to_gps(p_r, p)
@@ -89,5 +99,4 @@ def get_path():
 #def page_not_found(e):
 #  return render_template('404.html', title="404"), 404
 
-if __name__ == "__main__":
-	app.run(debug=True)
+app.run(debug=True)
